5d809e64af6c85d84242a16f51295029384d5be3
Estebanap <estebanap@ufps.edu.co>
2023-06-02 19:26:46 -0500

    First commit
    Initializing

---
+++ src/main/java/com/OvaEducativaAPI/OvaEducativaApiApplication.java
@@ -0,0 +1,13 @@
+package com.OvaEducativaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvaEducativaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvaEducativaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OvaEducativaAPI/OvaEducativaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OvaEducativaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvaEducativaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
